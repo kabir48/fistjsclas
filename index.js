74c8551c64b842fc51abfad49c93bf1ebdb5a252
kabir48 <h.kabir1498@gmail.com>
2022-11-09 02:09:48 +0600

    second part

---
+++ index.js
@@ -0,0 +1,16 @@
+// let num = ['Helloo', 'sokina', 'kamal'];
+// if (num[0].length - 1 == 4) {
+//     console.log(`You are Allowed`);
+// } else {
+//     console.log(`You are not Allowed`);
+// }
+
+let num2 = [1, 3, 4, 5, 6, 7, 8, 9, 1, 10];
+for (el of num2) {
+
+    if (el > 5) {
+        console.log(el)
+
+    }
+
+}
