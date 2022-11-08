bc33e8d9915719f08bf32e5f5baf93c868b506e7
kabir48 <h.kabir1498@gmail.com>
2022-11-09 02:36:33 +0600

    index file

--- index.js
+++ index.js
@@ -5,12 +5,16 @@
 //     console.log(`You are not Allowed`);
 // }
 
-let num2 = [1, 3, 4, 5, 6, 7, 8, 9, 1, 10];
-for (el of num2) {
+// let num2 = [1, 3, 4, 5, 6, 7, 8, 9, 1, 10];
+// for (el of num2) {
 
-    if (el > 5) {
-        console.log(el)
+//     if (el > 5) {
 
-    }
+//         console.log(el)
+//     }
 
-}
+// }
+
+let a = ["Banana", "Orange", "Apple", "Mango"]
+a.push('pitha', 'juice')
+console.log(a)
